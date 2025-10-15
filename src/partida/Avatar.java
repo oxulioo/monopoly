79d075351cdc94a5b8d9ca2a57ad4b8fc7394923
oxulioo <xulianbc.n1@gmail.com>
2025-10-15 19:05:23 +0200

    mierda

--- src/partida/Avatar.java
+++ src/partida/Avatar.java
@@ -5,35 +5,7 @@ import monopoly.*;
 import java.util.ArrayList;
 
 
-public class Avatar {
-
-    //Atributos
-    private String id; //Identificador: una letra generada aleatoriamente.
-    private String tipo; //Sombrero, Esfinge, Pelota, Coche
-    private Jugador jugador; //Un jugador al que pertenece ese avatar.
-    private Casilla lugar; //Los avatares se sitúan en casillas del tablero.
-
-    //Constructor vacío
-    public Avatar() {
-
-
-        this.jugador = ; //hace falta el this pq jugador llama a avatar y avatar a jugador
-
-    }
-
-    /*Constructor principal. Requiere éstos parámetros:
-    * Tipo del avatar, jugador al que pertenece, lugar en el que estará ubicado, y un arraylist con los
-    * avatares creados (usado para crear un ID distinto del de los demás avatares).
-     */
-    public Avatar(String tipo, Jugador jugador, Casilla lugar, ArrayList<Avatar> avCreados) {
-        this.tipo = tipo;
-        this.jugador = jugador;
-        this.lugar = lugar;
-        generarId(avCreados); //genera el nuevo id no repetido a la lista de los avatares creados
-
-    }
-
-    //A continuación, tenemos otros métodos útiles para el desarrollo del juego.
+//A continuación, tenemos otros métodos útiles para el desarrollo del juego.
     /*Método que permite mover a un avatar a una casilla concreta. Parámetros:
     * - Un array con las casillas del tablero. Se trata de un arrayList de arrayList de casillas (uno por lado).
     * - Un entero que indica el numero de casillas a moverse (será el valor sacado en la tirada de los dados).
